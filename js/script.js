7a14cfd02c36d78ac8a0bdc82ee2923a8fea63ae
Michał Sawicki <sawicki_michal@wp.pl>
2018-03-11 11:14:02 +0100

    add arr

---
+++ js/script.js
@@ -0,0 +1,16 @@
+var femaleNames = ['Asia', 'Kasia', 'Ola', 'Jola'];
+var maleNames = ['Piotrek', 'Marek', 'Arek', 'Jarek'];
+
+var allNames = concat(femaleNames,maleNames);
+
+console.log(allNames);
+
+var newName = 'Marian';
+
+if (allNames.indexOf(newName) === -1){
+    console.log('Parametr prawidłowy');
+} else {
+    console.log('Parametr nieprawidłowy');
+}
+
+var arr = allNames.push('Marian');
